911494c0f2388cc03b8cf029488bdf5a795ff2d0
Wagner Alves <alvesdasilvawagner1997@gmail.com>
2021-04-06 19:20:09 -0300

    Iniciando autenticação no login

--- frontend/src/templates/login/Login.js
+++ frontend/src/templates/login/Login.js
@@ -1,32 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 import useForms from './useForms'
+import verifyDatas from './verifyDatas'
 import './index.css';
 
 function Login(){
 
     const [{values}, handleChange, handleSubmit] = useForms();
-
-    const verifyEmail = (email) => {
-        return new Promise((resolve)=> {
-            if(email !== '' && email !== null && email !== undefined){
-               if(email.indexOf("@") !== -1){
-                    return resolve({status: true})
-               }else{
-                    return resolve({status: false})
-               }
-            }else {
-                return resolve({status: false})
-            }
-        })
-    }
+    const [verifyPassword, verifyEmail] = verifyDatas();
+    const [{classFormEmail, statusFormEmail}, setClassFormEmail ] = useState({classFormEmail: "", statusFormEmail: true});
+    const [{classFormPassword, statusFormPassword}, setClassFormPassword ] = useState({classFormPassword: "", statusFormPassword: true});
 
     const authentication = () => {
         verifyEmail(values.email)
             .then(e => {
                if(e.status){
-                    alert('Email válido')
+                    setClassFormEmail({classFormEmail: "input-formLogin-Success", statusFormEmail: true});
+                    verifyPassword(values.password)
+                        .then(e => {
+                            if(e.status){
+                                setClassFormPassword({classFormPassword: "input-formLogin-Success", statusFormPassword: true});
+                            }else{
+                                setClassFormPassword({classFormPassword: "input-formLogin-Error", statusFormPassword: false});
+                            }
+                        })
                 }else {
-                    alert("Email inválido")
+                    setClassFormEmail({classFormEmail: "input-formLogin-Error", statusFormEmail: false});
+                    setClassFormPassword({classFormPassword: "input-formLogin-Error", statusFormPassword: false});
                }
             })
     }
@@ -35,8 +34,18 @@ function Login(){
             <div className="telaLogin">
                 <h3>Acesse sua conta</h3>
                 <form onSubmit={handleSubmit(authentication)}>
-                    <input onChange={handleChange} type="text" name="email" id="email" placeholder="E-mail"/>
-                    <input onChange={handleChange} type="password" name="password" id="password" placeholder="Senha"/>
+                    <>
+                        {!statusFormEmail && (
+                            <span id="messageErrorEmail">Endereço de e-mail inválido!</span>
+                        )}
+                    </>
+                    <input onChange={handleChange} type="text" className={classFormEmail} name="email" id="email" placeholder="E-mail"/>
+                    <>
+                        {!statusFormPassword && (
+                            <span id="messageErrorPassword">Senha inválida ou incorreta!</span>
+                        )}
+                    </>
+                    <input onChange={handleChange} type="password" className={classFormPassword} name="password" id="password" placeholder="Senha"/>
                     <input type="submit" value="ENTRAR" />
                 </form>
             </div>
